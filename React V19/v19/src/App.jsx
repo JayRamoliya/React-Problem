35c3b999a566d96f7bee087924af60e61700649a
Jay Ramoliya <ramoliyajay9@gmail.com>
2024-06-19 09:50:39 +0530

    RJ Video 9

--- React V19/v19/src/App.jsx
+++ React V19/v19/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import NetfilxCard from './components/NetfilxCard';
 
 const App = () => {
   return (
     <>
-      Hello Jay Ramoliya
+      <h1>Hello Jay Ramoliya</h1>
+      <NetfilxCard/>
     </>
   )
 }
 
 export default App
+
+
+console.log(React.createElement('h1', null, "Hello Jay Ramoliya"));
