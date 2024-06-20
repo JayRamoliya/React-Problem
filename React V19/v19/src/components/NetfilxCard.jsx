0e36ee824a6daf3ae908fd56d316021bea24dd65
Jay Ramoliya <ramoliyajay9@gmail.com>
2024-06-20 09:58:22 +0530

    RJ Video 13

--- React V19/v19/src/components/NetfilxCard.jsx
+++ React V19/v19/src/components/NetfilxCard.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import seriesData from '../api/seriesData.json'
 
+
+// export const NetfilxCard = () => {
 const NetfilxCard = () => {
     let name = "Naruto Uzumaki"
     let rating = 10.0
@@ -11,15 +14,28 @@ const NetfilxCard = () => {
     return (
         // [<NetfilxCard key={1}/>,<NetfilxCard key={2}/>] don't use like this use <></>
         <>
-            <div onClick={clickimg}>
-                <img src="Naruto_newshot.webp" alt="" />
-            </div>
-            <h2>{name}</h2>
-            <h3>Rating:{rating}</h3>
-            <p>
-                Naruto Uzumaki (うずまきナルト, Uzumaki Naruto) is a shinobi of Konohagakure's Uzumaki clan. He became the jinchūriki of the Nine-Tails on the day of his birth — a fate that caused him to be shunned by most of Konoha throughout his childhood. After joining Team Kakashi, Naruto worked hard to gain the village's acknowledgement all the while chasing his dream to become Hokage. In the following years, through many hardships and ordeals, he became a capable ninja, regarded as a hero both by the villagers, and soon after, the rest of the world, becoming known as the Hero of the Hidden Leaf (木ノ葉隠れの英雄, Konohagakure no Eiyū, literally meaning: Hero of the Hidden Tree Leaves).
-            </p>
-            <button>{ age>=18 ? 'watch now' : 'not watch'}</button>
+            <ul>
+                {seriesData.map((current) => {
+                    return (
+                        <li key={current.id}>
+                            <div onClick={clickimg}>
+                                <img src={current.img_url} alt=""  width={300}/>
+                            </div>
+
+                            <h2>{current.name}</h2>
+
+                            <h3>Rating:{current.rating}</h3>
+                            <p>{current.description}</p>
+                            <p>Genre : {current.genre}</p>
+                            <p>Cast : {current.cast}</p>
+
+                            <a href={current.watch_url}>
+                                <button>{age >= 18 ? 'watch now' : 'not watch'}</button>
+                            </a>
+                        </li>
+                    )
+                })}
+            </ul>
         </>
     )
 }
