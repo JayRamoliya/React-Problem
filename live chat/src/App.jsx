758f73dc9eed31c24c4453e9ae7b7bf6d5159bc4
Jay Ramoliya <ramoliyajay9@gmail.com>
2024-07-04 09:33:31 +0530

    RJ Chat app

---
+++ live chat/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserLogin from './components/UserLogin'
+import ChatContainer from './components/ChatContainer'
+
+const App = () => {
+  return (
+    <>
+      <UserLogin/>
+      <ChatContainer/>
+    </>
+  )
+}
+
+export default App
